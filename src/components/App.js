18904a9f098d1c9ff149afd843d3dc1fac3634a7
Ashishkalbande11 <ashishkalbande6@gmail.com>
2024-09-18 17:03:02 +0530

    ..

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,10 @@ const App = () => {
   const[count, setCount] = useState(0);
   return (
     <div>
-        {/* Do not remove the main div */}
         <P>Button clicked {count} times</P>
         <button onClick = { () => {
           setCount(count+1)
         }}>Click me</button>
-
     </div>
   )
 }
